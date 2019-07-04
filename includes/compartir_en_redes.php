da558c31b600be67d065e384f894f7331905cce4
GandresCoello18 <goyeselcoca@gmail.com>
2019-07-03 22:24:50 -0500

    url para redes sociales

--- includes/compartir_en_redes.php
+++ includes/compartir_en_redes.php
@@ -4,17 +4,17 @@
 								</span>
 								
 								<div class="flex-wr-s-s size-w-0">
-									<a href="https://www.facebook.com/sharer/sharer.php?u=" class="dis-block f1-s-13 cl0 bg-facebook borad-3 p-tb-4 p-rl-18 hov-btn1 m-r-3 m-b-3 trans-03">
+									<a href="https://www.facebook.com/sharer/sharer.php?u=" class="facebook dis-block f1-s-13 cl0 bg-facebook borad-3 p-tb-4 p-rl-18 hov-btn1 m-r-3 m-b-3 trans-03">
 										<i class="fab fa-facebook-f m-r-7"></i>
 										Facebook
 									</a>
 
-									<a href="https://twitter.com/intent/tweet?text=[]&url=[url]&hashtags=[hashtag]" class="dis-block f1-s-13 cl0 bg-twitter borad-3 p-tb-4 p-rl-18 hov-btn1 m-r-3 m-b-3 trans-03">
+									<a href="https://twitter.com/intent/tweet?text=[]&url=[url]&hashtags=[hashtag]" class="twitter dis-block f1-s-13 cl0 bg-twitter borad-3 p-tb-4 p-rl-18 hov-btn1 m-r-3 m-b-3 trans-03">
 										<i class="fab fa-twitter m-r-7"></i>
 										Twitter
 									</a>
 
-									<a href="https://api.whatsapp.com/send?text=[text]" class="dis-block f1-s-13 cl0 bg-success borad-3 p-tb-4 p-rl-18 hov-btn1 m-r-3 m-b-3 trans-03">
+									<a href="https://api.whatsapp.com/send?text=[text]" class="whatsapp dis-block f1-s-13 cl0 bg-success borad-3 p-tb-4 p-rl-18 hov-btn1 m-r-3 m-b-3 trans-03">
 										<i class="fab fa-whatsapp m-r-7"></i>
 										Whatsapp
 									</a>
